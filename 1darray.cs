efa8daec69678f8285aafd6307372a2549a2d596
Rohan H K <rohanhk2@gmail.com>
2022-04-19 21:03:17 +0530

    commit

---
+++ 1darray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class OneD_Array
+    {
+        public static void Main()
+        {
+            int[] a = new int[100];
+            int i, n, sum = 0;
+
+            Console.WriteLine("Enter the number of elements to be stored: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element is- {0} : ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+            Console.Write("Sum of all elements stored in the array is : {0}\n\n", sum);
+        }
+    }
+}
