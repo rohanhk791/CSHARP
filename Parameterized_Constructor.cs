b10bf4484705b87857e8bcc84442eb70de13fa78
Rohan H K <rohanhk2@gmail.com>
2022-04-20 17:50:15 +0530

    commit

---
+++ Parameterized_Constructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramcons
+{
+    internal class param_constr
+    {
+        public static void Main()
+        {
+            Console.WriteLine("parameterized constructor");
+            Coordinate point = new Coordinate(10, 20);
+
+            Console.WriteLine(point.x); //output: 50  
+            Console.WriteLine(point.y); //output: 20  
+        }
+    }
+
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+}
