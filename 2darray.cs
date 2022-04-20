05f12c02a880a31262929e5a883ab853419e66b0
Rohan H K <rohanhk2@gmail.com>
2022-04-20 14:19:04 +0530

    commit

--- 2darray.cs
+++ 2darray.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task_22
+namespace arr
 {
     internal class TwoD_Array
     {
---
+++ string.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name
+{
+    internal class str_oper
+    {
+        public static void Main()
+        {
+            //creation of string
+            string str1 = "Rohan";
+            string str2 = "Gowda";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+
+            // get length of str1
+            int length = str1.Length;
+            Console.WriteLine("Length : " + length);
+
+            //concatinating two strings
+            string a = string.Concat(str1, str2);
+            Console.WriteLine("concating the two strings: " + a);
+
+            //comparison of two strings
+            Boolean b = str1.Equals(str2);
+            Console.WriteLine("str1 and str2 are equal: " + b);
+
+            //string interpolation
+            // create string
+            string name = "C# String";
+
+            // string interpolation
+            string message = $"Welcome to {name}";
+            Console.WriteLine(message);
+
+
+        }
+    }
+}
