b7bc543adb90f3abbfa34f27e5e71ef45615b555
Rohan H K <rohanhk2@gmail.com>
2022-04-20 14:14:43 +0530

    commit

---
+++ 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class _2
+    {
+
+        public static void Main()
+        {
+            int a=2, b=4; 
+            Console.WriteLine("Before swap a= " + a + " b= " +b);
+            a = a * b; 
+            b = a / b; 
+            a = a / b; 
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+
+    }
---
+++ emp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp
+{
+  
+    public class em
+    {
+        int Tid;
+        string Tname;
+
+        
+        public void getinfo()
+        {
+            Console.WriteLine("Enter Trainee ID");
+            Tid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Trainee Name");
+            Tname = Console.ReadLine();
+            
+        }
+
+        
+        public void display()
+        {
+            Console.WriteLine("Trainee ID    : {0}", Tid);
+            Console.WriteLine($"Trainee Name : {Tname}");
+            
+        }
+
+        public static void Main()
+        {
+            int n;
+            trainee t = new trainee();
+            //Asking for how many number of records to be entered
+            Console.WriteLine("Enter the number of Trainees");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //for loop 
+            for (int i = 0; i < n; i++)
+            {
+        
+                t.getinfo();
+                Console.WriteLine("");
+                
+                t.display();
+                Console.WriteLine("");
+                Console.WriteLine("Trainee Details Added Successfully");
+
+
+            }
+        }
+    }
+}
+
