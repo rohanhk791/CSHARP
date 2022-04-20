f82e06aad75d0f4c693b4ae7c16ad58a3521c997
Rohan H K <rohanhk2@gmail.com>
2022-04-20 17:15:12 +0530

    commit

--- param_array.cs
+++ param_array.cs
@@ -4,30 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Csharp
+// Write a program to perform sum of natural numbers using params array.
+
+namespace Concepts
 {
-    internal class param_array
+    public class paramarray
     {
-        public static void OrderDetails(params int[] a)
+        public static void paramsarray(params int[] arr)
         {
-            foreach (int i in a)
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(i);
+                sum += arr[i];
+
             }
-            Console.WriteLine("**************************");
+            Console.WriteLine("Sum of natural numbers is {0}", sum);
 
         }
+
         public static void Main()
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
-            //First Call
-            OrderDetails(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
-            //Second Call
-            OrderDetails(1, 2, 3, 4, 5, 6, 7);
-            //Third Call
-            OrderDetails(numbers);
+            int n;
+
+            Console.WriteLine("Enter the size of the array");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] num = new int[n];
+
+            Console.WriteLine("Enter the Values");
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            paramsarray(num);
+            Console.Write("\n\n");
+
 
         }
-
     }
 }
