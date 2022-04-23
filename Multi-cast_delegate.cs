b23d0ccfe22e62def8c553e641d57fc6d0b4cfc0
Rohan H K <rohanhk2@gmail.com>
2022-04-23 14:47:00 +0530

    commit

---
+++ Multi-cast_delegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+        namespace MulticastDelegate
+    {
+        public delegate void MathDelegate(int No1, int No2);
+        public class Program
+        {
+            public static void Add(int x, int y)
+            {
+                Console.WriteLine("The sum is : " + (x + y));
+            }
+            public static void Sub(int x, int y)
+            {
+                Console.WriteLine("The sub is : " + (x - y));
+            }
+            public void Mul(int x, int y)
+            {
+                Console.WriteLine("The mul is : " + (x * y));
+            }
+            public void Div(int x, int y)
+            {
+                Console.WriteLine("The div is : " + (x / y));
+            }
+
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                MathDelegate del1 = new MathDelegate(Add);
+                MathDelegate del2 = new MathDelegate(Program.Sub);
+                MathDelegate del3 = p.Mul;
+                MathDelegate del4 = new MathDelegate(p.Div); 
+                MathDelegate del5 = del1 + del2 + del3 + del4;
+                del5.Invoke(20, 5);
+                Console.WriteLine();
+                del5 -= del2;
+                del5(22, 7);
+
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ multiple_catch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplecatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,x;
+            Console.WriteLine("enter two number");
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("c value = " + c);
+            }
+            catch (DivideByZeroException dbze)
+            {
+                Console.WriteLine("second number should not be zero");
+                Console.Write("Do you wanna stay in Generic Calculator [Y/N] : ");
+                x = Convert.ToChar(Console.ReadLine());
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("enter only integer numbers");
+                Console.Write("Do you wanna stay in Generic Calculator [Y/N] : ");
+                x = Convert.ToChar(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
