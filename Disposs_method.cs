f245c6c9f0d42506fb5390ad33d110cc4defdc55
Rohan H K <rohanhk2@gmail.com>
2022-04-27 15:09:28 +0530

    commit

--- Disposs_method.cs
+++ Disposs_method.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Csharp
 {
-    internal class Disposs_method
+    class dispose_method : IDisposable
     {
+
+
+        public void Dispose()
+        {
+            Console.WriteLine("this is dispose method()");
+        }
+
+
+    }
+    class sub
+    {
+        public sub()
+        {
+            int a = 10, b = 5, c = a - b;
+            Console.WriteLine("{0} - {1} = {2}", a, b, c);
+
+        }
+        ~sub()
+        {
+            Console.WriteLine("Im destructor");
+        }
+
+
+    }
+    class demo
+    {
+        public static void Main()
+        {
+            sub a = new sub();
+            dispose_method d = new dispose_method();
+            d.Dispose();
+
+        }
     }
 }
---
+++ value.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class passbyvalue
+    {
+        public static void Main()
+        {
+            Console.WriteLine("this is a pass by value");
+            int X = 100;
+            Console.WriteLine("Variable value before calling the method: " + X);
+            AddingNumberByFive(X);
+            Console.WriteLine("Variable value after calling the method: " + X);
+            Console.WriteLine("enter to exit");
+            Console.ReadLine();
+       
+        Console.WriteLine("this is a pass by reference");
+            int value = 100;
+        Console.WriteLine("Variable value before calling the method: " + value);
+            AddingNumberByFive(ref value);
+        Console.WriteLine("Variable value after calling the method: " + value);
+            Console.WriteLine("enter to exit");
+            Console.ReadLine();
+            
+        public static void AddingNumberByFive(int number)
+        {
+            number = number + 5;
+            Console.WriteLine("Variable value inside the method : " + number);
+        }
+
+        {
+            Console.WriteLine("this is a pass by reference");
+            int value = 100;
+            Console.WriteLine("Variable value before calling the method: " + value);
+            AddingNumberByFive(ref value);
+            Console.WriteLine("Variable value after calling the method: " + value);
+            Console.WriteLine("enter to exit");
+            Console.ReadLine();
+        }
+        public static void AddingNumberByFive(ref int value)
+        {
+            value = value + value;
+            Console.WriteLine("Variable value inside the method : " + value);
+        }
+
+    }
+}
